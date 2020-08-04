18d81c8b9230642913655236763349f722402474
nest-code <ernestoadreyulian@gmail.com>
2020-08-04 20:09:36 +0700

    Tugas 11 - Reactjs State & Components Life Cycle

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import HargaBuah from './tugas11/HargaBuah'; // Tugas 11
+import Timer from './tugas12/Timer'; // Tugas 11
 
 function App() {
   return (
     <div className="App">
-      <HargaBuah />
+       { /* Tugas 11 */ }
+      {/* <HargaBuah /> */}
+      { /* Tugas 12 */ }
+      
+      <Timer waktu = {100}/>
     </div>
   );
 }
---
+++ my-app/src/tugas12/Timer.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            time: this.props.waktu,
+            date: new Date().toLocaleTimeString()
+        }
+    }
+
+    componentDidMount() {
+        if (this.props.start !== undefined) {
+            this.setState({
+                time: this.props.start
+            });
+        }
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            time: this.state.time - 1
+        });
+    }
+
+    render() {
+        return(
+            this.state.time > 0 ?  
+            <div>         
+                <h1 style={{textAlign: "center"}}>Sekarang Jam : {this.state.date}.    <span style={{marginRight: "20px"}}></span>       Hitung mundur : {this.state.time}</h1>
+            </div>
+            :
+            <>
+            </>
+        )
+    }
+}
+
+export default Timer;
